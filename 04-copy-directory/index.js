fc04b1beab072e52b3323469d670e68ad5f00301
Growler42 <taras_ter@icloud.com>
2021-11-05 03:12:11 +0300

    feat: implement 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,36 @@
+const path = require('path');
+const {mkdir, copyFile, unlink} = require('fs');
+const {readdir} = require('fs/promises');
 
+const filesPath = path.resolve(__dirname, 'files');
+const filesCopyPath = path.resolve(__dirname, 'files-copy');
+
+const isErrCallback = err => {
+  if (err) {
+    console.error(err);
+  }
+};
+
+mkdir(filesCopyPath, {recursive: true}, isErrCallback);
+
+let filesArr = [];
+
+readdir(filesPath)
+  .then((files) => {
+    filesArr = files;
+    for (const file of files) {
+      copyFile(path.resolve(filesPath, file),
+        path.resolve(filesCopyPath, file), isErrCallback);
+    }
+  })
+  .catch(isErrCallback);
+
+readdir(filesCopyPath)
+  .then((files) => {
+    for (const file of files) {
+      if (filesArr.indexOf(file) === -1) {
+        unlink(path.resolve(filesCopyPath, file), isErrCallback);
+      }
+    }
+  })
+  .catch(isErrCallback);
